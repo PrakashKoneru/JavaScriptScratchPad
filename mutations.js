a729284d258f45707074d65ecc73c416fa57c07f
Prakash Koneru <PrakashKoneru@Sivas-MacBook-Pro.local>
2016-09-16 19:15:38 -0400

    Falsy Bouncer

---
+++ mutations.js
@@ -0,0 +1,27 @@
+// Question
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+// Solution
+
+function mutation(arr) {
+  var sec = arr[1].toLowerCase().split("");
+  var fir = arr[0].toLowerCase().split("");
+  for (var i=0; i< sec.length; i++){
+    var index = fir.indexOf(sec[i]);
+    if (index === -1){
+      console.log("x");
+      return false;
+    }
+  }
+return true;
+  
+}
+
+mutation(["hello", "hey"]);
